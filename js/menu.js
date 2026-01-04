73b3049b3e94a0299e64c0e504711f992f3d8595
Escotilhadotempo <escotilhadotempo@gmail.com>
2026-01-04 18:20:01 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,16 @@
+let ultimoScroll = 0;
+const topo = document.querySelector('.topo');
 
+window.addEventListener('scroll', () => {
+    const scrollAtual = window.pageYOffset;
+
+    if (scrollAtual > ultimoScroll && scrollAtual > 100) {
+        // rolando para baixo → esconde menu
+        topo.style.top = "-120px";
+    } else {
+        // rolando para cima → mostra menu
+        topo.style.top = "0";
+    }
+
+    ultimoScroll = scrollAtual;
+});
